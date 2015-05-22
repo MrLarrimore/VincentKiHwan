aa6bdfb18abf04949eba66b7de2995152551e2f1
Vincent Lonero <vincent.lonero.16@gmail.com>
2015-05-22 10:56:23 -0700

    deleted everything

--- php/controller/file_viewer.php
+++
@@ -1,6 +0,0 @@
-<?php
- $filename = base64_decode($_GET['file']);
-    // Check the folder location to avoid exploit
-    if (dirname($filename) == '/home/user/Pictures')
-        echo file_get_contents($filename);
-

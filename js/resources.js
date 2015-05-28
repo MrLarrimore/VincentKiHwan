fc9265961392537eb0234840fa3b1c860254f92f
Vincent Lonero <vincent.lonero.16@gmail.com>
2015-05-28 11:17:27 -0700

    Final COmmit of HS
    Files Ki Hwan needs for his computer

---
+++ js/resources.js
@@ -0,0 +1,54 @@
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+        
+
+	 {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+	 {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+         {name: "player", type:"image", src: "data/img/level1ani.png"},
+         {name: "playerX", type:"image", src: "data/img/level2ani.png"},
+         {name: "tower", type:"image", src: "data/img/tower_round.svg.png"},      
+         {name: "creep1", type:"image", src: "data/img/brainmonster.png"},    
+         {name: "creep2", type:"image", src: "data/img/gloop.png"},         
+         {name: "title-screen", type:"image", src: "data/img/title.png"},     
+         {name: "exp-screen", type:"image", src: "data/img/loadpic.png"},   
+         {name: "gold-screen", type:"image", src: "data/img/spend.png"},   
+         {name: "load-screen", type:"image", src: "data/img/loadpic.png"},   
+         {name: "new-screen", type:"image", src: "data/img/newpic.png"},  
+         {name: "spear", type:"image", src: "data/img/spear.png"},
+         {name: "black-screen", type:"image", src: "data/img/blackBackground.png"},
+         {name: "enemyHero", type:"image", src: "data/img/elf.png"},
+         {name: "jump", type:"image", src: "data/img/jump.png"},
+         {name: "select", type:"image", src: "data/img/select.png"},
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+
+        {name: "level1", type: "tmx", src: "data/map/Awesomenauts1.tmx"},
+
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+	 */	
+
+        {name: "play-bgm", type: "audio", src: "data/bgm/"},
+        {name: "menu-bgm", type: "audio", src: "data/bgm/"},
+        {name: "jump-sound", type: "audio", src: "data/bgm/"},
+
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+	 */
+];
